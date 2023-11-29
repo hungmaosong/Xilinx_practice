56c492309ea48ef01da2d454a4848b413ce35b1f
king <maosong0710@gmail.com>
2023-11-29 22:37:20 +0800

    lab2 test OK

---
+++ lab2/lab2/project_1/project_1.sdk/lab2_gpio_test/Debug/src/main.d
@@ -0,0 +1,43 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/sleep.h
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab2_gpio_test_bsp/ps7_cortexa9_0/include/sleep.h:
---
+++ lab2/lab2_HW.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <xgpio.h>
+#include "xparameters.h"
+#include "sleep.h"
+
+#define Green 0
+#define Yellow 1
+#define Red 2
+
+void simple_delay(int simple_delay)
+{
+   volatile int i = 0;
+   for (i = 0; i < simple_delay; i++)
+      ;
+}
+
+int main()
+{
+   XGpio gpio0, gpio1;
+   int led = 0;
+   int switch_data;
+   int button_data;
+
+   int timer;
+   int state;
+
+   XGpio_Initialize(&gpio0, XPAR_AXI_GPIO_0_DEVICE_ID); // initialize input XGpio variable
+   XGpio_Initialize(&gpio1, XPAR_AXI_GPIO_1_DEVICE_ID); // initialize output XGpio variable
+
+   XGpio_SetDataDirection(&gpio0, 1, 0x0); // set first channel tristate buffer to output (LED)
+   XGpio_SetDataDirection(&gpio0, 2, 0xF); // set second channel tristate buffer to input (switch)
+
+   XGpio_SetDataDirection(&gpio1, 1, 0xF); // set first channel tristate buffer to input (button)
+   
+   //reset(button0)
+   while (1)
+   {
+      button_data = XGpio_DiscreteRead(&gpio1, 1); // get button data
+      switch_data = = XGpio_DiscreteRead(&gpio0, 2); // get switch data
+      if (button_data == 0b00001)
+      {
+         if (switch_data == 0b00000001)
+         {
+            state = Green;
+            timer = 7;
+            break;
+         }
+         else
+         {
+            state = Green;
+            timer = 15;
+            break;
+         }
+      }
+   }
+   
+   while(1){
+      switch_data = XGpio_DiscreteRead(&switch_gpio, 1) // get switch data
+
+      if(switch_data & 0b00000001 == 0)
+      {
+         simple_delay(100000000);
+
+         timer--;
+         if(timer == 0)
+         {
+            if(state == Green)
+            {
+               state = Yellow;
+               timer = 1;
+            }
+            else if (state == Yellow)
+            {
+               state = Red;
+               timer = 8;
+            }
+            else
+            {
+               state = Green;
+               timer = 7;
+            }            
+         }
+      }
+      else
+      {
+         simple_delay(100000000);
+
+         timer--;
+         if (timer == 0)
+         {
+            if (state == Green)
+            {
+               state = Yellow;
+               timer = 1;
+            }
+            else if (state == Yellow)
+            {
+               state = Red;
+               timer = 16;
+            }
+            else
+            {
+               state = Green;
+               timer = 15;
+            }
+         }
+      }
+      
+     
+   }
+   return 0;
+}
